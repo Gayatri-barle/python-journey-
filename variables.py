d2c449a27e302f2945d49f1b1c3bae957a72395b
Gayatri Barle <barlegayatri6@gmail.com>
2026-09-20 23:18:28 +0530

    we learn variables in python

---
+++ variables.py
@@ -0,0 +1,32 @@
+# 20/09/2026
+
+# VARIABLES are container that store values, through the variable we can access the values again whenever we want.
+# syntax:  VARIABLE_NAME = VALUES 
+
+a = "gayatri barle"
+b = 6
+print(a, b)
+print(b)
+
+
+# we can assign values to the variable like this 
+num =  2.1 
+num1 = 85
+num2 = 'hello'
+
+# is_num = true  / NameError if the first alpha is small 
+
+is_num = True
+
+# now we have to know about the data which kind of data we store in variables.
+# let check the types of data  through the type() function.
+
+type(num2) # this code is run but we need to print the type of data so, we use print function.
+
+print(type(num2))   # <class 'str'>
+print(type(is_num))   # <class 'bool'>
+print(type(num))   #  <class 'float'>
+print(type(num1))   #  <class 'int'>
+
+# now we go to another topic that is datatype ,
+# we should have to know about the datatype 
